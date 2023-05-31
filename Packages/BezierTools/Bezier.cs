fa5d625775c717a78bcdf28d374d27aaf3501889
momentchan <mingjyunhung@gmail.com>
2023-05-31 15:15:01 +0800

    bezier texture bake

--- Packages/BezierTools/Bezier.cs
+++ Packages/BezierTools/Bezier.cs
@@ -9,5 +9,23 @@ namespace BezierTools
     {
         [SerializeField]
         public BezierData data;
+
+        public Texture2D BakeToTexture(int sampleCount)
+        {
+            var rt = new Texture2D(sampleCount, 2, TextureFormat.RGBAFloat, false);
+            rt.wrapMode = TextureWrapMode.Clamp;
+
+            for (int i = 0; i < sampleCount; i++)
+            {
+                var nt = (i * sampleCount) % 1;
+                var pos = data.PositionNormalizeT(nt);
+                var vel = data.VelocityNormalizeT(nt);
+                rt.SetPixel(i, 0, new Color(pos.x, pos.y, pos.z, 0));
+                rt.SetPixel(i, 1, new Color(vel.x, vel.y, vel.z, 0));
+            }
+            rt.Apply();
+
+            return rt;
+        }
     }
 }
