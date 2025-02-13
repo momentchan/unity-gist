b9cb3c129fecb4fe0a58dc0805ef4fa324f26bda
momentchan <mingjyunhung@gmail.com>
2025-02-13 11:40:05 +0900

    add fps displayer

---
+++ Scripts/Utilities/FPSDisplay.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+    private float deltaTime = 0.0f;
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+    }
+
+    void OnGUI()
+    {
+        int width = Screen.width, height = Screen.height;
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, width, height * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = height * 2 / 100;
+        style.normal.textColor = Color.white;
+        float fps = 1.0f / deltaTime;
+        string text = $"{fps:0.} FPS";
+        GUI.Label(rect, text, style);
+    }
+}
